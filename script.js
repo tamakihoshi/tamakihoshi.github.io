71603d767977816cac152589b02f25418e416230
thoshi13 <tamakihoshi24@gmail.com>
2025-06-08 21:09:43 +0900

    add starter files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const sticky = document.querySelector('.sticky');
+const stickyParent = document.querySelector('.sticky-parent');
+const sections = document.querySelectorAll('.scrolly-text');
+
+const scrollWidth = sticky.scrollWidth;
+const verticalScrollHeight = stickyParent.getBoundingClientRect().height - sticky.getBoundingClientRect().height -1500;
+
+function horizontalScroll() {
+  //Checking whether the sticky element has entered into view or not
+  let stickyPosition = sections[2].getBoundingClientRect().top;
+  if(stickyPosition > 0){
+      return;
+  }else{
+      let scrolled = sections[2].getBoundingClientRect().top; //how much is scrolled?
+      sticky.scrollLeft =(scrollWidth/verticalScrollHeight)*(-scrolled)*0.85;
+  }
+}
+
+function updateVisibleSections() {
+  const scrollY = window.scrollY;
+  const sectionHeight = window.innerHeight;
+  const offset = 0;
+  const index = Math.floor((scrollY + offset) / sectionHeight);
+
+  sections.forEach((section, i) => {
+    if (i === index) {
+      section.classList.add('visible');
+    } else if( i === 2 &&  index > 2 ){
+      section.classList.add('visible');
+    } else {
+      section.classList.remove('visible');
+    }
+  });
+}
+
+
+
+// Single scroll event listener calls both handlers
+window.addEventListener('scroll', () => {
+  horizontalScroll();
+  updateVisibleSections();
+});
+
+
+
+function openPDFPopup(url) {
+    document.getElementById('frame').src = url;
+    document.getElementById('pdfModal').style.display='block';
+  }
+
+
+
+
